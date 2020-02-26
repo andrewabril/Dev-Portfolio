2cb66a7580174640d3a9edaa38a3b6759030fca7
andrewabril <abrila2794@gmail.com>
2020-02-25 21:28:03 -0500

    finished skeleton of portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: ContactComponent }
+];
 
 @NgModule({
   declarations: [
@@ -17,8 +25,11 @@ import { ContactComponent } from './contact/contact.component';
     ContactComponent
   ],
   imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     FormsModule,
-    BrowserModule.withServerTransition({ appId: 'serverApp' })
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
